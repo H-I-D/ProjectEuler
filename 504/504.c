a0729d5347162974626810e0bbe4fbe099070d3e
Hideaki Kanehara <sss_ftr.hid@icloud.com>
2016-02-21 14:06:29 +0900

    504

---
+++ 504/504.c
@@ -0,0 +1,39 @@
+#include <tgmath.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// time ./a.out => 20s
+
+// Euclidean Algorithm (http://edu.clipper.co.jp/pg-2-44.html)
+int gcd(int x, int y){
+	if(y < 1) exit(0);  // error
+	int z;
+	if(x < y){
+		z = x;
+		x = y;
+		y = z;
+	}
+	if(x % y == 0) return y;
+	return gcd(y, x % y);
+}
+
+int is_square(int a, int b, int c, int d){
+	int lp_contains = ((a+c)*(b+d)/2) - (gcd(a,b) + gcd(b,c) + gcd(c,d) + gcd(d,a)) / 2 + 1;
+	return pow((int)sqrt(lp_contains), 2) == lp_contains;
+}
+int main(){
+	int m = 100;
+	// printf("input m: "); scanf("%d", &m);
+	// int quadrilaterals = pow(m,4);
+	int lp_square = 0; // answer
+	for(int a = 1; a <= m; a++){
+		for(int b = 1; b <= m; b++){
+			for(int c = 1; c <= m; c++){
+				for(int d = 1; d <= m; d++){
+					if(is_square(a, b, c, d)) lp_square++;
+				}
+			}
+		}
+	}
+	printf("answer: %d\n", lp_square);
+}
---
+++ 504/504.rb
@@ -0,0 +1,54 @@
+# Problem 504
+
+# time ruby 504.rb => real: 1m30s
+
+#############################
+######## functions ##########
+#############################
+
+# def get_area(a, b, c, d)
+#   return (a+c) * (b+d) / 2
+# end
+
+# def get_LP_side(a, b, c, d)
+  # lp_side = 4  # vertex
+  # for i in 1..m do
+  #   lp_side += 1 if (i % a == 0 || b % a == 0) && -b/a * i + b > 0 && -b/a * i < 0
+  #   lp_side += 1 if (i % d == 0 || a % d == 0) && -a/d * i + a > 0 && -a/d * i < 0
+  #   lp_side += 1 if (i % c == 0 || d % c == 0) && -d/c * i + d > 0 && -d/c * i < 0
+  #   lp_side += 1 if (i % b == 0 || c % b == 0) && -c/b * i + c > 0 && -c/b * i < 0
+  # end
+  #=> bad algorithm
+# 	return a.gcd(b) + b.gcd(c) + c.gcd(d) + d.gcd(a)
+# end
+
+# Pick's theorem
+# def get_LP(area, lp_side)
+#   return area - lp_side/2 + 1
+# end
+
+def is_square(a, b, c, d)
+  lp_contains = ((a+c)*(b+d)/2) - (a.gcd(b) + b.gcd(c) + c.gcd(d) + d.gcd(a)) / 2 + 1
+  return Math.sqrt(lp_contains).to_i ** 2 == lp_contains
+end
+
+###############################
+###############################
+
+# print 'input m: '
+# m = gets.to_i;
+m = 100
+# quadrilaterals_all = m ** 4  # (m - 1 + 1) ** 4
+
+lp_square = 0  # answer
+for a in 1..m do
+  for b in 1..m do
+    for c in 1..m do
+      for d in 1..m do
+        lp_square += 1 if is_square(a, b, c, d)
+      end
+    end
+  end
+end
+
+puts "#{lp_square} quadrilaterals strictly contain a square number of lattice points in m = #{m}"
