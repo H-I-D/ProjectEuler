8032dcf52ad16d1095d85ca3e5b64b5d5fcf9050
Hideaki Kanehara <sss_ftr.hid@icloud.com>
2016-02-24 00:11:25 +0900

    001

--- 001/001.rb
+++ 001/001.rb
@@ -1,10 +1,6 @@
 # Problem 001
-
 # 0.077[s]
 
 sum = 0
-for i in 1..999 do
-  sum += i if i%3 == 0 || i%5 == 0 && i%15 != 0
-end
-
+1.upto(999){|i| sum += i if i%3 == 0 || i%5 == 0 && i%15 != 0 }
 puts "Sum: #{sum}"
